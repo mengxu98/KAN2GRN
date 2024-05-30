070d5cf955cd8d86037e7ea331affcba354a5867
Meng xu <86040828+mengxu98@users.noreply.github.com>
2024-05-30 17:48:33 +0800

    initial kan2grn

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+# python -m venv pykan-env
+# source pykan-env/bin/activate  # On Windows use `pykan-env\Scripts\activate`
+pip install git+https://github.com/KindXiaoming/pykan.git
+git clone https://github.com/KindXiaoming/pykan.git
+pip install -r pykan/requirements.txt
